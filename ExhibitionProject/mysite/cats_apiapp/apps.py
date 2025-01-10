39b84aa2a125f10d8711e6b8dae146c454f8c04f
Shcherbakov Denis <130468887+originaldenis@users.noreply.github.com>
2025-01-10 15:08:58 +0400

    Delete ExhibitionProject directory

--- ExhibitionProject/mysite/cats_apiapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CatsApiappConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "cats_apiapp"
--- ExhibitionProject/mysite/cats_apiapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Kittens(models.Model):
-    name = models.CharField(max_length=10)
-    color = models.CharField(max_length=30)
-    age = models.IntegerField(default=0)
-    description = models.TextField(max_length=100)
-    breed = models.CharField(max_length=50)
